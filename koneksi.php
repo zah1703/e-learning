9423320ad43fe0a5d820e4eccd1b172131ae6220
Zahrotul Hidayah <114077782+zah1703@users.noreply.github.com>
2023-05-30 21:41:06 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php 
 $koneksi = mysqli_connect("localhost","root","","mahasiswa");
- 
+
 // Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
+if (mysqli_connect_error()){
+	echo "Koneksi database berhasil : " .  mysqli_connect_error();
 }
- 
+
 ?>
